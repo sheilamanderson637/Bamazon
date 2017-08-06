32eadac42011ea9e4cc308488ac8fe56e8e66385
Sheila M Anderson <sheilamanderson@sheilas-air.attlocal.net>
2017-08-06 11:53:46 -0500

    Completed basic JS connection to DB

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+var table = require('cli-table');
 
 // Establish connection
 var connection = mysql.createConnection({
@@ -11,8 +13,12 @@ var connection = mysql.createConnection({
 // Show connection and ID
 connection.connect(function(err) {
   if (err) throw err;
-  connection.query ('select * from products', function(err, data) {
-     if (err) throw err;
-  console.log(data);
-});
+console.log("connected as id" + connection.threadID);
+
+// Logs all of the items in the database
+connection.query('SELECT * FROM products', function(err,res) {
+  if (err) throw err;
+  console.log(res);
+})
+
 });
