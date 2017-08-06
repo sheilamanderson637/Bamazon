b196dcbfe95960a4b94c925b96d9062d8c930035
Sheila M Anderson <sheilamanderson@sheilas-air.attlocal.net>
2017-08-06 18:18:27 -0500

    updated comments to be clearer

--- bamazon.js
+++ bamazon.js
@@ -1,3 +1,4 @@
+// Dependancies
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var Table = require('cli-table');
@@ -11,6 +12,7 @@ var connection = mysql.createConnection({
     password: 'root',
     database: 'bamazon_DB'
 });
+
 // Show connection and ID
 connection.connect(function(err) {
 if (err) throw err;
@@ -18,14 +20,14 @@ console.log("connected as id " + connection.threadId);
 start();
 });
 
-// // Logs all of the items in the database
+// // Logs all of the items in the database ** used for intial testing **
 // connection.query('SELECT * FROM products', function(err,res) {
 // if (err) throw err;
 // console.log(res);
 // start();
 //   })
 
-// performs inital query of Products table from database
+// performs inital query of products table from database
 var start = function(a) {
     connection.query('SELECT * FROM products', function(err, res) {
         console.log(' ');
@@ -34,7 +36,8 @@ var start = function(a) {
         console.log('                       BAMAZON PRODUCTS AVAILABLE FOR PURCHASE');
         console.log(' ');
         console.log('**************************************************************************************');
-        // New Table instance to format returned sql data
+
+// New Table to format returned sql data
             var table = new Table({
                 head: ['Item ID #', 'Product Name', 'Department Name', 'Price', 'Quantity'],
                 colWidths: [10, 40, 10, 10, 10]
@@ -47,8 +50,8 @@ var start = function(a) {
         purchaseItem();
         });
     };
-// Shopping prompt to purchase items
 
+// Shopping prompt to purchase items
 var purchaseItem = function() {
     inquirer.prompt([{
         name: "Item",
@@ -79,22 +82,27 @@ var purchaseItem = function() {
         }
         }]).then(function(answer) {
             var ItemInt = parseInt(answer.Qty);
-                //Queries the database
+
+// Queries the database
                 connection.query("SELECT * FROM products WHERE ?", [{id: answer.Item}], function(err, data) { 
                     if (err) throw err;
-                    //Checks if sufficient quantity exists
+
+// Verify if there is enough inventory to fill the order
                     if (data[0].stock_qty < ItemInt) {
                        console.log("We're sorry, that Item is currently out of stock.\n");
                        console.log("Please choose another product.\n");
                        purchaseItem(); 
                     } else {
-                        //if quantity exists updates database
+
+// If there is enough stock to fulfill the order, update the database
                         var updateQty = data[0].stock_qty - ItemInt;
                         var totalPrice = data[0].price * ItemInt;
                         connection.query('UPDATE products SET stock_qty = ? WHERE id = ?', [updateQty, answer.Item], function(err, results) {
                         if(err) {
                             throw err;
                         } else {
+
+// Order confirmation messaging to customer
                         console.log('Your purchase was successfull!\n');
                         console.log('The total cost for your order is: $ ' + totalPrice);
                         console.log('Thank you for shopping at Bamazon! Please return soon.')
